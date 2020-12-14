ba9e4317ab5559fe33ffa12b3f1b57b37dd5bc64
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2020-12-14 23:38:48 +0100

    return on 2 min

---
+++ Assets/Custom/Script/FlowController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FlowController : MonoBehaviour
+{
+
+    public float timeToWait = 120;
+    [SerializeField]
+    protected UnityEvent ActionEvent;
+
+    void Start() {
+        StartCoroutine(waitToEndScene(timeToWait));
+    }
+
+    IEnumerator waitToEndScene(float time) {
+        yield return new WaitForSeconds(time);
+        ActionEvent.Invoke();
+    }
+
+}
