c3d6d8748a9affe6eee0d46e4ec946329a48ad18
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2020-12-15 21:35:36 +0100

    extras 2 & 3

---
+++ Assets/Custom/Script/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public float velocity;
+    Transform _transform;
+    void Awake() {
+        _transform = GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        _transform.localEulerAngles = _transform.localEulerAngles + Time.deltaTime * mouseSensitivity * new Vector3 (-mouseY, mouseX, 0.0f);
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        _transform.Translate(new Vector3(horizontal,0.0f, vertical) * Time.deltaTime * velocity);
+    }
+}
