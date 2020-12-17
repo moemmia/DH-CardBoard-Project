bf10fef835c66ee0bd7a82ddd1a1e5066a610a25
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2020-12-17 10:56:44 +0100

    sphere movement

---
+++ Assets/Custom/Script/SphereMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+    protected Transform _transform;
+
+    public float size = 1;
+
+    void Awake() {
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update() {
+        _transform.Translate(new Vector3(Mathf.Sin(Time.time), Mathf.Cos(Time.time), .0f) * Time.deltaTime * size);
+    }
+}
